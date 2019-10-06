4fec5beb33bdbacfded46cd848eb51c3476cd5f8
Ben Meredith <ben.meredith@gmail.com>
2019-10-06 17:56:21 -0400

    remove the not now link from the done page

--- includes/views/welcome/_footer.php
+++ includes/views/welcome/_footer.php
@@ -1,6 +1,6 @@
             </div>
 
-            <?php if ( 'bctt-grow' !== bctt_get_step() ): ?>
+            <?php if ( 'bctt-done' !== bctt_get_step() ): ?>
                 <a 
                     href="<?php echo admin_url( 'options-general.php?page=better-click-to-tweet' ); ?>"
                     class="text-gray-600 underline w-full text-center block mb-10 text-xs"
@@ -9,14 +9,6 @@
                 </a>
             <?php endif; ?>
             
-            <?php if ( 'bctt-grow' === bctt_get_step()  ): ?>
-                <a 
-                    href="<?php echo bctt_get_step_url( 'bctt-done' ); ?>"
-                    class="text-gray-600 underline w-full text-center block mb-10 text-xs"
-                    title="<?php _e( 'Go to dashboard', 'better-click-to-tweet' )?>">
-                <?php _e( 'Not right now', 'better-click-to-tweet' )?>
-                </a>
-            <?php endif; ?>
 
     </body>
 </html>
