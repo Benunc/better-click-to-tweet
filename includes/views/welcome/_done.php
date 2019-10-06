9555d19797af69456e4e0ee1fb44a6a81f31c48a
Ben Meredith <ben.meredith@gmail.com>
2019-10-06 17:55:48 -0400

    make the done link more descriptive

--- includes/views/welcome/_done.php
+++ includes/views/welcome/_done.php
@@ -19,7 +19,7 @@
     href="<?php echo admin_url( 'options-general.php?page=better-click-to-tweet' ); ?>"
     class="text-blue-500 underline w-full text-center block"
     title="<?php _e( 'Go to dashboard', 'better-click-to-tweet' ); ?>">
-        <?php _e( 'Get Started', 'better-click-to-tweet' ); ?>
+        <?php _e( 'Back to the WordPress Settings', 'better-click-to-tweet' ); ?>
     </a>
 
     <a 
