c40e28ea3f8ee6ee519644ce93144a853a746297
Ben Meredith <ben.meredith@gmail.com>
2019-10-15 16:38:16 -0400

    new Youtube vid

--- includes/views/welcome/_welcome.php
+++ includes/views/welcome/_welcome.php
@@ -7,7 +7,7 @@
             <iframe 
                 width="560" 
                 height="315" 
-                src="https://www.youtube.com/embed/4GU9KLwtEWM?controls=0" 
+                src="https://www.youtube.com/embed/G-N9uZPq6o8?controls=0?modestbranding=1&rel=0" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen>
