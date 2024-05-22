4cf431c6707c1fd7f50be47c8fad91299f177f6c
Ben Meredith <ben.meredith@gmail.com>
2024-05-22 16:35:05 -0400

    ran the build

--- assets/block/build/index.asset.php
+++ assets/block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '187d36cfcd1b3bc0281f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '982d37f6e97c5de1b646');
