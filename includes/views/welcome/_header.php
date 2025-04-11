0b1157e0809a314bfdf8b7ab303c288a7624dd6d
Ben Meredith <benmeredith@Bens-2024-MacBook-Pro.local>
2025-04-11 00:52:43 -0400

    Replace welcome screen logo with SVG version

--- includes/views/welcome/_header.php
+++ includes/views/welcome/_header.php
@@ -11,45 +11,45 @@
 
     <body class="bg-gray-200">
         <img
-            src="<?php echo plugins_url( '../../assets/img/bctt-logo-alt.png', dirname(__FILE__) ); ?>" 
-            alt="<?php _e( 'Better Click To Tweet', 'better-click-to-tweet' )?>" 
+            src="<?php echo plugins_url( '../../assets/svg/better.svg', dirname(__FILE__) ); ?>"
+            alt="<?php _e( 'Better Click To Tweet', 'better-click-to-tweet' )?>"
             class="mx-auto w-9/12 sm:max-w-xs mt-12 mb-10" />
 
             <div id="bctt-steps-nav" class="hidden md:block max-w-2xl mx-6 md:mx-auto">
                 <ol class="bctt-steps">
-                    <li 
+                    <li
                         <?php echo bctt_get_step_class( 'bctt-setup' ); ?>>
-                        <a 
+                        <a
                             href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=1")?>"
                             title="<?php _e( 'Back to that Awe-inspiring video', 'better-click-to-tweet' )?>">
                             <?php _e( 'Welcome', 'better-click-to-tweet' )?>
                         </a>
                     </li>
                     <li <?php echo bctt_get_step_class( 'bctt-usage' ); ?>>
-                        <a 
-                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-usage")?>" 
+                        <a
+                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-usage")?>"
                             title="<?php _e( 'Learn how to use Better Click To Tweet', 'better-click-to-tweet' )?>">
                             <?php _e( 'Usage', 'better-click-to-tweet' )?>
                         </a>
                     </li>
                     <li <?php echo bctt_get_step_class( 'bctt-content' ); ?>>
-                        <a 
-                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-content")?>" 
+                        <a
+                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-content")?>"
                             title="<?php _e( 'Learn why it\'s probably not us, it\'s your content', 'better-click-to-tweet' )?>">
                             <?php _e( 'Content', 'better-click-to-tweet' )?>
                         </a>
                     </li>
                     <li <?php echo bctt_get_step_class( 'bctt-grow' ); ?>>
-                        <a 
-                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-grow")?>" 
+                        <a
+                            href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-grow")?>"
                             title="<?php _e( 'Sign up to be a part of the (hashtag) NextBigThing', 'better-click-to-tweet' )?>">
                             <?php _e( 'Grow', 'better-click-to-tweet' )?>
                         </a>
                     </li>
                     <li <?php echo bctt_get_step_class( 'bctt-done' ); ?>>
-                        <a 
+                        <a
                             href=" <?php echo admin_url( "admin.php?page=bctt-welcome&step=bctt-done")?>"
-                            title="<?php _e( 'Why you tryna skip steps?', 'better-click-to-tweet' )?>">                        
+                            title="<?php _e( 'Why you tryna skip steps?', 'better-click-to-tweet' )?>">
                             <?php _e( 'Done!', 'better-click-to-tweet' )?>
                         </a>
                     </li>
