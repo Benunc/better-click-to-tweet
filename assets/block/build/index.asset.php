292263156fdd967b6d48bd3458d6cb518a4f262e
Ben Meredith <ben.meredith@gmail.com>
2026-03-05 15:23:59 -0500

    updates to some build tools

--- assets/block/build/index.asset.php
+++ assets/block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'f7ebb57247c1a71feeaa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'bb8c9d3316a3b09f9336');
